f01d8a03bf60b5ab72be4b6138a3e9ca08fec57d
krish ranghani <krishranghani@gmail.com>
2023-08-28 17:21:39 +0530

    28-8-2023

---
+++ c program/testclass.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+class TEST {
+  private:
+    int TestCode;
+    string Description;
+    int NoCandidate;
+    int CenterReqd;
+
+  public:
+    int CALCNTR() {
+      return NoCandidate /(100 + 1);
+    }
+
+    void SCHEDULE() {
+      cout << "Enter Test Code: ";
+      cin >> TestCode;
+      cout << "Enter Description: ";
+      cin >> Description;
+      cout << "Enter Number of Candidates: ";
+      cin >> NoCandidate;
+      CenterReqd = CALCNTR();
+    }
+
+    void DISPTEST() {
+      cout << "Test Code: " << TestCode << endl;
+      cout << "Description: " << Description << endl;
+      cout << "Number of Candidates: " << NoCandidate << endl;
+      cout << "Number of Centers Required: " << CenterReqd << endl;
+    }
+};
+int main ()
+{
+     TEST stu; // stu is a student class object
+     stu.CALCNTR();
+     stu.SCHEDULE();
+     stu.DISPTEST();
+    return 0;
+}
